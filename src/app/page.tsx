6f2eda233eef0a9916c61104014b5f4640d0db1f
Van <losion445@gmail.com>
2025-10-04 11:01:43 -0700

    first commit && mvp

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+import { prisma } from '../../lib/script';
+
+
+export default async function Home() {
+  const posts = await prisma.post.findMany({
+    where: {published: true},
+    orderBy: {createdAt: "desc"}
+  });
+
+
+  return (
+    <div className="p-8 max-w-2xl mx-auto">
+    <h1>My blog</h1>
+    {posts.map(post => (
+      <article key={post.id}>
+        <a href={`/posts/${post.id}`}>
+          <h2 className="text-4xl font-bold mb-4">{post.title}</h2>
+        </a>
+        <p className="text-gray-600 mb-6">{post.content}</p>
+        <small>{post.createdAt.toLocaleDateString()}</small>
+      </article>
+    ))}
+    </div>
+  );
+}
---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,23 @@
+import { prisma } from "@/../lib/script";
+
+import { notFound } from "next/navigation";
+
+export default async function Post({params}: {params: {id: string}}) {
+	const post = await prisma.post.findUnique({
+		where: {id: parseInt(params.id)}
+	});
+
+	if (!post || !post.published) {
+		notFound();
+	}
+
+	return (
+    <div className="p-8 max-w-2xl mx-auto">
+      <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
+      <p className="text-gray-600 mb-6">
+        {post.createdAt.toLocaleDateString()}
+      </p>
+      <div className="prose">{post.content}</div>
+    </div>
+  );
+}
